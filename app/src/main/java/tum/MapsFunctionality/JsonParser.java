21aa026c214f302d0c85da6489586bd8c25f7a1b
Philipp Zetterer <p.zetterer@live.com>
2017-07-14 13:59:09 +0200

    changing Androidstudio
    migrate to android studio 2.3.3
    migrate gradl,
    change to api 25

--- app/src/main/java/tum/MapsFunctionality/JsonParser.java
+++ app/src/main/java/tum/MapsFunctionality/JsonParser.java
@@ -1,9 +1,5 @@
 package tum.MapsFunctionality;
 
-import android.net.http.RequestQueue;
-
-import com.google.android.gms.appdatasearch.GetRecentContextCall;
-
 /**
  * Created by Philipp on 7/12/2017.
  */
